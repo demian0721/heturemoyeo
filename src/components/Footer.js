8556c27caaf08203227f91c3aca427982c8c345c
yoojin-kim90 <erthard@naver.com>
2021-07-29 09:20:10 +0900

    fotter&button
    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,29 +17,36 @@ const Footer = () => {
     
     return (
       <React.Fragment>
-        <Menu>
-          <thead>
-            <tr>
-              <th><Text clickEvent={() => { history.push('/'); }} style={{ cursor: "pointer" }}>홈버튼</Text></th>
-              <th><Text clickEvent={() => { history.push('/'); }} style={{ cursor: "pointer" }}>모임구하기</Text></th>
-              <th><Text clickEvent={() => { history.push('/'); }} style={{ cursor: "pointer" }}>친구관리</Text></th>
-              <th><Text clickEvent={() => { history.push('/'); }} style={{ cursor: "pointer" }}>대화방</Text></th>
-              <th><Text clickEvent={() => { history.push('/mypage'); }} style={{ cursor: "pointer" }}>마이페이지</Text></th>
-            </tr>
-          </thead>
-        </Menu>
+        <Grid width="100%" height="10vh" bg="skyblue" style={{position:"fixed", bottom:"0",zIndex:"100", minHeight: "70px", maxHeight: "100px"}}>
+          <Menu>
+            <thead>
+              <tr>
+                <th><Text clickEvent={() => { history.push('/'); }} style={{ cursor: "pointer" }}>홈버튼</Text></th>
+                <th><Text clickEvent={() => { history.push('/'); }} style={{ cursor: "pointer" }}>모임구하기</Text></th>
+                <th><Text clickEvent={() => { history.push('/'); }} style={{ cursor: "pointer" }}>친구관리</Text></th>
+                <th><Text clickEvent={() => { history.push('/'); }} style={{ cursor: "pointer" }}>대화방</Text></th>
+                <th><Text clickEvent={() => { history.push('/mypage'); }} style={{ cursor: "pointer" }}>마이페이지</Text></th>
+              </tr>
+            </thead>
+          </Menu>
+        </Grid>
       </React.Fragment>
     );
 };
 
 
 const Menu = styled.table`
-    background-color: skyblue;
     width: 100%;
+<<<<<<< Updated upstream
     z-index: 1;
     position: fixed;
     bottom: 0;
     padding: 10pxg;
+=======
+    padding: 10px;
+    max-width: 800px;
+    margin: auto;
+>>>>>>> Stashed changes
     //styled component use
 `;
 

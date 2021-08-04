69055febb282b779502d597c7a3fd0b3a30d7f02
demian0721 <demian0721@naver.com>
2021-08-04 10:06:11 +0900

    오버레이 적용 완료
    리덕스 모듈 추가 및
    오버레이 데이터 출력 구현

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,13 @@
+import { useEffect } from 'react'
+
+const useOutsideClick = (ref, handler) => {
+    useEffect(() => {
+        const listener = (event) => {
+            if (!ref?.current || (ref && ref?.current?.contains?.(event.target))) return; handler(event);
+        };
+        document.addEventListener('mousedown', listener);
+        return () => { document.removeEventListener('mousedown', listener) }
+    }, [ref, handler])
+}
+
+export default useOutsideClick
--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -7,6 +7,7 @@ import thunk from 'redux-thunk';
 // REDUCER
 import User from './modules/user';
 import Post from './modules/post';
+import Marker from './modules/marker';
 
 // HISTORY
 export const history = createBrowserHistory();
@@ -14,7 +15,8 @@ export const history = createBrowserHistory();
 const rootReducer = combineReducers({
     user: User,
     post: Post,
-    router: connectRouter(history),
+    marker: Marker,
+    router: connectRouter(history)
 });
 
 const middlewares = [thunk.withExtraArgument({ history })];

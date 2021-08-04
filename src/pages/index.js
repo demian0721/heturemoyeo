3e121864a8654354ada92ccdb97631dcedc140f2
demian0721 <demian0721@naver.com>
2021-08-05 04:24:09 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,31 @@
-import Main from './Main';
-import Login from './Login';
-import Signup from './Signup';
-import Mypage from './Mypage';
-import MypageEdit from './MypageEdit';
-import Terms from './Terms';
-import SignupInfo from './SignupInfo';
-import PostList from './PostList';
-import PostListMy from './PostListMy';
-import PostDetail from './PostDetail';
-import PostWrite from './PostWrite';
-import PostModify from './PostModify';
-import MProfile from './MemberProfile';
+import Main from "./Main";
+import Login from "./Login";
+import Signup from "./Signup";
+import Mypage from "./Mypage";
+import MypageEdit from "./MypageEdit";
+import Terms from "./Terms";
+import SignupInfo from "./SignupInfo";
+import PostList from "./PostList";
+import PostListMy from "./PostListMy";
+import PostDetail from "./PostDetail";
+import PostWrite from "./PostWrite";
+import PostModify from "./PostModify";
+import MProfile from "./MemberProfile";
+import ChatList from "./ChatList";
 
-export { Main, Login, Signup, SignupInfo, Mypage, MypageEdit, Terms, PostList, PostListMy, PostDetail, PostWrite, PostModify, MProfile };
+export {
+  Main,
+  Login,
+  Signup,
+  SignupInfo,
+  Mypage,
+  MypageEdit,
+  Terms,
+  PostList,
+  PostListMy,
+  PostDetail,
+  PostWrite,
+  PostModify,
+  MProfile,
+  ChatList,
+};

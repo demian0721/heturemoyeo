967b55721030427357b447b8d53d7023c0b31da7
demian0721 <demian0721@naver.com>
2021-08-12 19:59:41 +0900

    중간저장
    ㅇㅇ

--- src/utils/Logger.js
+++ src/utils/Logger.js
@@ -1,18 +1,42 @@
 const getDate = () => {
-  const dateNow = new Date()
-  const year = dateNow.getFullYear()
-  const month = String(dateNow.getMonth() + 1).length === 1 ? `0${dateNow.getMonth() + 1}` : dateNow.getMonth() + 1
-  const day = String(dateNow.getDay()).length === 1 ? `0${dateNow.getDay()}` : dateNow.getDay()
-  const hours = String(dateNow.getHours()).length === 1 ? `0${dateNow.getHours()}` : dateNow.getHours()
-  const minutes = String(dateNow.getMinutes()).length === 1 ? `0${dateNow.getMinutes()}` : dateNow.getMinutes()
-  const seconds = String(dateNow.getSeconds()).length === 1 ? `0${dateNow.getSeconds()}` : dateNow.getSeconds()
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
-}
+  const dateNow = new Date();
+  const year = dateNow.getFullYear();
+  const month =
+    String(dateNow.getMonth() + 1).length === 1
+      ? `0${dateNow.getMonth() + 1}`
+      : dateNow.getMonth() + 1;
+  const day =
+    String(dateNow.getDay()).length === 1
+      ? `0${dateNow.getDay()}`
+      : dateNow.getDay();
+  const hours =
+    String(dateNow.getHours()).length === 1
+      ? `0${dateNow.getHours()}`
+      : dateNow.getHours();
+  const minutes =
+    String(dateNow.getMinutes()).length === 1
+      ? `0${dateNow.getMinutes()}`
+      : dateNow.getMinutes();
+  const seconds =
+    String(dateNow.getSeconds()).length === 1
+      ? `0${dateNow.getSeconds()}`
+      : dateNow.getSeconds();
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+};
 
-export default {
-  info: (prefix, message) => console.log(`%c${getDate()} [INFO] ${prefix}` + ' ' + `%c${message}`, 'color: lightGreen;', 'color: white'),
-  debug: (prefix, message) => console.log(`%c${getDate()} [DEBUG] ${prefix}` + ' ' + `%c${message}`, 'color: cyan;', 'color: white'),
-  warn: (prefix, message) => console.log(`%c${getDate()} [WARN] ${prefix}` + ' ' + `%c${message}`, 'color: orange;', 'color: white'),
-  error: (prefix, message) => console.log(`%c${getDate()} [ERRRO] ${prefix}` + ' ' + `%c${message}`, 'color: red;', 'color: white'),
-  verbose: (prefix, message) => console.log(`%c${getDate()} [VERBOSE] ${prefix}` + ' ' + `%c${message}`, 'color: gray;', 'color: white'),
-}
+const getMessage = (message) => {
+  const regex = /\[(.*?)\]/gm;
+  const firstMatch = message.match(regex).shift();
+  const result = message.split(firstMatch).filter((el) => el.length !== 0).map((el) => el.trim()).join(" ")
+  return `${firstMatch} %c${result}`
+};
+
+const Logger = {
+  info: (message) => console.log(`%c${getDate()} [INFO] ${getMessage(message)}`, "color: lightGreen;", "color: white"),
+  debug: (message) => console.log(`%c${getDate()} [DEBUG] ${getMessage(message)}`, "color: cyan;", "color: white"),
+  warn: (message) => console.log(`%c${getDate()} [WARN] ${getMessage(message)}`, "color: orange;", "color: white"),
+  error: (message) => console.log(`%c${getDate()} [ERRRO] ${getMessage(message)}`, "color: red;", "color: white"),
+  verbose: (message) => console.log(`%c${getDate()} [VERBOSE] ${getMessage(message)}`, "color: gray;", "color: white"),
+};
+
+export default Logger;

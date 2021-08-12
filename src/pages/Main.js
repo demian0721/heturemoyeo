6eaa5489d74e17acb7f31c03e24921c3b70bab23
demian0721 <demian0721@naver.com>
2021-08-12 14:37:21 +0900

    Update Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -412,10 +412,13 @@ const Main = (props) => {
             <Button
               shadow="rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;"
               bg="rgba(255, 255, 255, 1)"
+              hoverBg="#16C59B"
               color="#16C59B"
+              hoverColor="#fcfcfc"
               padding="12px"
               margin="0 0 10px"
               radius="100%"
+              className="custom_transition"
               clickEvent={() =>
                 panTo(props?.coords?.latitude, props?.coords?.longitude)
               }

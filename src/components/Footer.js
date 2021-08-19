1da997465364c25bd9e0b7464911775902e448e5
demian0721 <demian0721@naver.com>
2021-08-19 13:53:49 +0900

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,61 +26,221 @@ const Footer = (props) => {
 
     return (
       <React.Fragment>
-        <Grid is_flex="center" width="100%" minWidth="280px" height="55px" bg="#FFF"
-        style={{position:"fixed", bottom:"0", zIndex:"2", borderTop:"0.4px solid #767676"}}>
+        <Grid
+          is_flex="center"
+          width="100%"
+          minWidth="280px"
+          height="55px"
+          bg="#FFF"
+          style={{
+            position: "fixed",
+            bottom: "0",
+            zIndex: "2",
+            borderTop: "0.4px solid #767676",
+          }}
+        >
+          {title == "home" ? (
+            <Grid
+              width="70px"
+              padding="7px 0px 5px 0px"
+              align="center"
+              color="#16C59B"
+              hoverColor="#16C59B"
+              style={{ display: "block" }}
+              clickEvent={() => {}}
+            >
+              <Link to="/">
+                <HomeOutlinedIcon />
+                <p>
+                  <text style={{ fontSize: "11px", fontWeight: "bold" }}>
+                    홈
+                  </text>
+                </p>
+              </Link>
+            </Grid>
+          ) : (
+            <Grid
+              width="70px"
+              padding="7px 0px 5px 0px"
+              align="center"
+              color="#7B7B7B"
+              hoverColor="#16C59B"
+              style={{ display: "block" }}
+              clickEvent={() => {}}
+            >
+              <Link to="/">
+                <HomeOutlinedIcon />
+                <p>
+                  <text style={{ fontSize: "11px", fontWeight: "bold" }}>
+                    홈
+                  </text>
+                </p>
+              </Link>
+            </Grid>
+          )}
 
-          {title=='home'? <Grid width="70px" padding="7px 0px 5px 0px" align="center" color="#16C59B" hoverColor="#16C59B" style={{display:"block"}} clickEvent={() => {}}>
-            <Link to='/'>
-              <HomeOutlinedIcon/>
-              <p><text style={{fontSize:"11px", fontWeight:"bold"}}>홈</text></p>
-            </Link>
-          </Grid> : <Grid width="70px" padding="7px 0px 5px 0px" align="center" color="#7B7B7B" hoverColor="#16C59B" style={{display:"block"}} clickEvent={() => {}}>
-            <Link to='/'>
-              <HomeOutlinedIcon/>
-              <p><text style={{fontSize:"11px", fontWeight:"bold"}}>홈</text></p>
-            </Link>
-          </Grid>}
+          {title == "group" ? (
+            <Grid
+              width="70px"
+              padding="7px 0px 5px 0px"
+              align="center"
+              color="#16C59B"
+              hoverColor="#16C59B"
+              style={{ display: "block" }}
+              clickEvent={() => {}}
+            >
+              <Link to="/postlist">
+                <SearchOutlinedIcon />
+                <p>
+                  <text style={{ fontSize: "11px", fontWeight: "bold" }}>
+                    모임
+                  </text>
+                </p>
+              </Link>
+            </Grid>
+          ) : (
+            <Grid
+              width="70px"
+              padding="7px 0px 5px 0px"
+              align="center"
+              color="#7B7B7B"
+              hoverColor="#16C59B"
+              style={{ display: "block" }}
+              clickEvent={() => {}}
+            >
+              <Link to="/postlist">
+                <SearchOutlinedIcon />
+                <p>
+                  <text style={{ fontSize: "11px", fontWeight: "bold" }}>
+                    모임
+                  </text>
+                </p>
+              </Link>
+            </Grid>
+          )}
 
-          {title=='group'? <Grid width="70px" padding="7px 0px 5px 0px" align="center" color="#16C59B" hoverColor="#16C59B" style={{display:"block"}} clickEvent={() => {}}>
-            <Link to='/postlist'>
-              <SearchOutlinedIcon/>
-              <p><text style={{fontSize:"11px", fontWeight:"bold"}}>모임</text></p>
-            </Link>
-          </Grid> : <Grid width="70px" padding="7px 0px 5px 0px" align="center" color="#7B7B7B" hoverColor="#16C59B" style={{display:"block"}} clickEvent={() => {}}>
-            <Link to='/postlist'>
-              <SearchOutlinedIcon/>
-              <p><text style={{fontSize:"11px", fontWeight:"bold"}}>모임</text></p>
-            </Link>
-          </Grid>}
+          {title == "friend" ? (
+            <Grid
+              width="70px"
+              padding="7px 0px 5px 0px"
+              align="center"
+              color="#16C59B"
+              hoverColor="#16C59B"
+              style={{ display: "block" }}
+              clickEvent={() => {
+                window.alert("준비 중인 서비스 입니다!");
+              }}
+            >
+              <PeopleAltOutlinedIcon />
+              <p>
+                <text style={{ fontSize: "11px", fontWeight: "bold" }}>
+                  친구
+                </text>
+              </p>
+            </Grid>
+          ) : (
+            <Grid
+              width="70px"
+              padding="7px 0px 5px 0px"
+              align="center"
+              color="#7B7B7B"
+              hoverColor="#16C59B"
+              style={{ display: "block" }}
+              clickEvent={() => {
+                window.alert("준비 중인 서비스 입니다!");
+              }}
+            >
+              <PeopleAltOutlinedIcon />
+              <p>
+                <text style={{ fontSize: "11px", fontWeight: "bold" }}>
+                  친구
+                </text>
+              </p>
+            </Grid>
+          )}
 
-          {title=='friend'? <Grid width="70px" padding="7px 0px 5px 0px" align="center" color="#16C59B" hoverColor="#16C59B" style={{display:"block"}} clickEvent={() => { window.alert('준비 중인 서비스 입니다!') }}>
-            <PeopleAltOutlinedIcon/>
-            <p><text style={{fontSize:"11px", fontWeight:"bold"}}>친구</text></p>
-          </Grid> : <Grid width="70px" padding="7px 0px 5px 0px" align="center" color="#7B7B7B" hoverColor="#16C59B" style={{display:"block"}} clickEvent={() => { window.alert('준비 중인 서비스 입니다!') }}>
-            <PeopleAltOutlinedIcon/>
-            <p><text style={{fontSize:"11px", fontWeight:"bold"}}>친구</text></p>
-          </Grid>}
+          {title == "chat" ? (
+            <Grid
+              width="70px"
+              padding="7px 0px 5px 0px"
+              align="center"
+              color="#16C59B"
+              hoverColor="#16C59B"
+              style={{ display: "block" }}
+              clickEvent={() => {}}
+            >
+              <Link to="/chat">
+                <SmsOutlinedIcon />
+                <p>
+                  <text style={{ fontSize: "11px", fontWeight: "bold" }}>
+                    대화방
+                  </text>
+                </p>
+              </Link>
+            </Grid>
+          ) : (
+            <Grid
+              width="70px"
+              padding="7px 0px 5px 0px"
+              align="center"
+              color="#7B7B7B"
+              hoverColor="#16C59B"
+              style={{ display: "block" }}
+              clickEvent={() => {}}
+            >
+              <Link to="/chat">
+                <SmsOutlinedIcon />
+                <p>
+                  <text style={{ fontSize: "11px", fontWeight: "bold" }}>
+                    대화방
+                  </text>
+                </p>
+              </Link>
+            </Grid>
+          )}
 
-          {title=='chat'? <Grid width="70px" padding="7px 0px 5px 0px" align="center" color="#16C59B" hoverColor="#16C59B" style={{display:"block"}} clickEvent={() => { history.push('/chat'); }}>
-            <SmsOutlinedIcon/>
-            <p><text style={{fontSize:"11px", fontWeight:"bold"}}>대화방</text></p>
-          </Grid> : <Grid width="70px" padding="7px 0px 5px 0px" align="center" color="#7B7B7B" hoverColor="#16C59B" style={{display:"block"}} clickEvent={() => { history.push('/chat'); }}>
-            <SmsOutlinedIcon/>
-            <p><text style={{fontSize:"11px", fontWeight:"bold"}}>대화방</text></p>
-          </Grid>}
-
-          {title=='profile'? <Grid width="70px" padding="7px 0px 5px 0px" align="center" color="#16C59B" hoverColor="#16C59B" style={{display:"block"}} clickEvent={() => { history.push('/mypage'); }}>
-            <FaceOutlinedIcon/>
-            <p><text style={{fontSize:"11px", fontWeight:"bold"}}>프로필</text></p>
-          </Grid> : <Grid width="70px" padding="7px 0px 5px 0px" align="center" color="#7B7B7B" hoverColor="#16C59B" style={{display:"block"}} clickEvent={() => { history.push('/mypage'); }}>
-            <FaceOutlinedIcon/>
-            <p><text style={{fontSize:"11px", fontWeight:"bold"}}>프로필</text></p>
-          </Grid>}
-
-      </Grid>
-      
-    </React.Fragment>
-  );
+          {title == "profile" ? (
+            <Grid
+              width="70px"
+              padding="7px 0px 5px 0px"
+              align="center"
+              color="#16C59B"
+              hoverColor="#16C59B"
+              style={{ display: "block" }}
+              clickEvent={() => {}}
+            >
+              <Link to="/mypage">
+                <FaceOutlinedIcon />
+                <p>
+                  <text style={{ fontSize: "11px", fontWeight: "bold" }}>
+                    프로필
+                  </text>
+                </p>
+              </Link>
+            </Grid>
+          ) : (
+            <Grid
+              width="70px"
+              padding="7px 0px 5px 0px"
+              align="center"
+              color="#7B7B7B"
+              hoverColor="#16C59B"
+              style={{ display: "block" }}
+              clickEvent={() => {}}
+            >
+              <Link to="/mypage">
+                <FaceOutlinedIcon />
+                <p>
+                  <text style={{ fontSize: "11px", fontWeight: "bold" }}>
+                    프로필
+                  </text>
+                </p>
+              </Link>
+            </Grid>
+          )}
+        </Grid>
+      </React.Fragment>
+    );
 };
 
 Footer.defaultProps = {
